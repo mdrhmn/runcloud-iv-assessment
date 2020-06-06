18e5a6eea3a9205aa3733ba8b49c87bdec4d591b
mdrhmn <50654608+mdrhmn@users.noreply.github.com>
2020-06-07 06:21:04 +0800

    Update server.php

--- runcloud/src/db/server.php
+++ runcloud/src/db/server.php
@@ -6,14 +6,12 @@ class Server {
 
     public $name;
     public $ipAddress;
-    public $connection_status;
     public $no_of_connections;
-    
+
     public function __construct()
     {
         $this->name = null;
         $this->ipAddress = null;
-        $this->connection_status = null;
         $this->no_of_connections = 0;
     }
 }
