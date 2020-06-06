eeeaaf7423da6ef489738ede32e6a4e727b65809
mdrhmn <50654608+mdrhmn@users.noreply.github.com>
2020-06-07 06:16:24 +0800

    Update ProPlan.php

--- runcloud/src/plan/types/ProPlan.php
+++ runcloud/src/plan/types/ProPlan.php
@@ -9,8 +9,8 @@ class ProPlan extends Plan {
     public function __construct()
     {
         parent::__construct();
-        $this->status = "ProPlan";
-        $this->set_status("ProPlan");
+        $this->status = "Pro Plan";
+        $this->set_status("Pro Plan");
     }
 
     public function set_status($newStatus)
